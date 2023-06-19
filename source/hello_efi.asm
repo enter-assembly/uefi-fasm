a83eba5749f8dc343438eedc6d9b01a832312f9a
Viktor Shpakovskiy <viktor_shpakovskiy@vk.com>
2023-06-19 14:45:01 +0700

    copypast fix

--- source/hello_efi.asm
+++ source/hello_efi.asm
@@ -9,15 +9,19 @@ include 'efi_table.inc'
 include 'efi_system_table.inc'
 include 'efi_simple_text_output_protocol.inc'
 include 'efi_console_out_table.inc'
+include 'efi_standard_error_table.inc'
  
 section '.text' code executable readable
 
 main:
     EfiSystemTable.Init
     EfiConsoleOut.Init
+    EfiStandardError.Init
 
+    EfiConsoleOut.EnableCursor 1
     EfiConsoleOut.ClearScreen
     EfiConsoleOut.OutputString hello_string
+    EfiStandardError.OutputString error_string
  
     jmp $
     retn
@@ -25,6 +29,7 @@ main:
 section '.data' data readable writeable
  
 hello_string du 'Hello, Uefi Fasm Library!!',13,10,0
+error_string du 'This is error string',13,10,0
 
 section '.reloc' fixups data discardable
 
