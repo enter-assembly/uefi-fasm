214952a20ef8a29e1ebc82294521a924e8c1222f
Viktor Shpakovskiy <viktor_shpakovskiy@vk.com>
2024-02-07 01:51:03 +0700

    Example update:
    - Изменён пример с целью упрощения демонстрации инициализации и использования библиотеки

--- source/hello_efi.asm
+++ source/hello_efi.asm
@@ -5,56 +5,69 @@ include '../include/efi_x64.inc'
 
 section '.data' data readable writeable
 
-include 'efi_table/efi_system_table.inc'
-include 'efi_table/efi_console_out_table.inc'
-include 'efi_table/efi_boot_services_table.inc'
+__SYSTEM_TABLE__    PTR EFI_SYSTEM
+__CONSOLE_OUTPUT__  PTR EFI_SIMPLE_TEXT_OUTPUT
+__CONSOLE_INPUT__   PTR EFI_SIMPLE_TEXT_INPUT
+__BOOT_SERVICES__   PTR EFI_BOOT_SERVICES
 
-__GOP__ PTR EFI_GRAPHICS_OUTPUT
-__GOP_GUID__ EFI_GUID EFI_GRAPHICS_OUTPUT_GUID
-__FRAME_BASE__ EFI_PHYSICAL_ADDRESS 0
+__GRAPHICS_OUTPUT_GUID__ EFI_GUID EFI_GRAPHICS_OUTPUT_GUID
+__GRAPHICS_OUTPUT__      PTR EFI_GRAPHICS_OUTPUT
+__FRAME_BUFFER_BASE__    PTR EFI_GRAPHICS_OUTPUT_BLT_PIXEL
 
 __BLUE__ EFI_GRAPHICS_OUTPUT_BLT_PIXEL 0xff, 0, 0
-__GREEN__ EFI_GRAPHICS_OUTPUT_BLT_PIXEL 0, 255, 0
-__RED__ EFI_GRAPHICS_OUTPUT_BLT_PIXEL 0, 0, 255
 
-__CONSOLE_IN PTR EFI_SIMPLE_TEXT_INPUT
 __INPUT_KEY EFI_INPUT_KEY
 
 section '.text' code executable readable
 
 main:
-    EfiSystemTable.Init
-    EfiConsoleOut.Init EfiSystemTable
-    EfiBootServices.Init EfiSystemTable
+; Initialize lib
+    mov [__SYSTEM_TABLE__], rdx
+    EFI_SYSTEM_Init Sys, __SYSTEM_TABLE__
 
-    EfiConsoleOut.ClearScreen
-    EfiConsoleOut.OutputString _looking_for_message
+    Sys.GetConsoleInTable
+    mov [__CONSOLE_INPUT__], rax
+    EFI_SIMPLE_TEXT_INPUT_Init Cin, __CONSOLE_INPUT__
 
-    EfiBootServices.LocateProtocol __GOP_GUID__, NULL, __GOP__
+    Sys.GetConsoleOutTable
+    mov [__CONSOLE_OUTPUT__], rax
+    EFI_SIMPLE_TEXT_OUTPUT_Init Cout, __CONSOLE_OUTPUT__
+
+    Sys.GetBootServicesTable
+    mov [__BOOT_SERVICES__], rax
+    EFI_BOOT_SERVICES_Init BootServices, __BOOT_SERVICES__
+
+; Locating EFI_GRAPHICS_OUTPUT table
+    Cout.ClearScreen
+    Cout.OutputString _looking_for_message
+
+    BootServices.LocateProtocol __GRAPHICS_OUTPUT_GUID__, NULL, __GRAPHICS_OUTPUT__
     call print_error
 
-    EFI_GRAPHICS_OUTPUT_Init Gop, __GOP__
-    Gop.ModeTable.GetFrameBufferBase
+; Initialize EFI_GRAPHICS_OUTPUT table
+    EFI_GRAPHICS_OUTPUT_Init Gout, __GRAPHICS_OUTPUT__
+
+; Getting frame buffer pointer
+    Gout.ModeTable.GetFrameBufferBase
+    mov [__FRAME_BUFFER_BASE__], rax
     
+; Filling frame buffer
     mov rcx, 4096 * 32
     xor rdx, rdx
     mov edx, [__BLUE__]
     .iter:
     mov [rax + rcx * 4], edx
     loop .iter
-    EfiConsoleOut.OutputString _ready
+    Cout.OutputString _ready
     jmp .ok
 
     .failed:
-    EfiConsoleOut.OutputString _failed
+    Cout.OutputString _failed
     jmp .exit
     .ok:
-    EfiConsoleOut.OutputString _ok
+    Cout.OutputString _ok
 
-    EfiSystemTable.GetConsoleInTable
-    mov [__CONSOLE_IN], rax
-    EFI_SIMPLE_TEXT_INPUT_Init Cin, __CONSOLE_IN
-
+; Reading from keyboard loop
     .read_key:
         Cin.ReadKeyStroke __INPUT_KEY
         test rax, rax
@@ -65,7 +78,7 @@ main:
     ; read ok
         mov ax, [__INPUT_KEY + EFI_INPUT_KEY.UnicodeChar]
         mov [__output_key], ax
-        EfiConsoleOut.OutputString __output_key
+        Cout.OutputString __output_key
         jmp .read_key
 
     .exit:
@@ -78,21 +91,21 @@ print_error:
     cmp rax, 0
     jne .fail
     pop rdx
-    EfiConsoleOut.OutputString __EFI_SUCCESS
-    EfiConsoleOut.OutputString _new_line
+    Cout.OutputString __EFI_SUCCESS
+    Cout.OutputString _new_line
     ret
         .fail:
     mov rdx, EFI_NOT_READY
     test rax, rdx
     jne @f
     pop rdx
-    EfiConsoleOut.OutputString __EFI_NOT_READY
-    EfiConsoleOut.OutputString _new_line
+    Cout.OutputString __EFI_NOT_READY
+    Cout.OutputString _new_line
     ret
     @@:
     pop rdx
-    EfiConsoleOut.OutputString __EFI_ERROR
-    EfiConsoleOut.OutputString _new_line
+    Cout.OutputString __EFI_ERROR
+    Cout.OutputString _new_line
     ret
 
 section '.data' data readable writeable
